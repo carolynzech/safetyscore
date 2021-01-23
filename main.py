823c4f93e469fa4a47f4716f44eca00650b0300c
Ezra Bernstein <mremdb@gmail.com>
2021-01-23 13:07:25 -0500

    Added more pages, data in session, gitignore
    I added a place to submit the data post request to, and am storing everything in session until we get a database going
    I also added a gitignore so it will stop tracking the pycache and future .env

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, request, render_template, redirect, session
 app = Flask(__name__)
 
+#a randomly generated key to encrypt the session cookies, will change later
+app.secret_key = b'\xfe\x14\x07-#\x1a\x7f\xbe#PG\xd0\x8eo\x1ea'
+
+#default page
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.rotue('/login')
+#login page
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method=='POST':
+        username = request.form['username']
+        password = request.form['password']
+        session['username'] = username
+        return render_template('home.html')
+    elif request.method=='GET':
+        return render_template('login.html')
 
+#home page, where you can submit the ratings?
 @app.route('/home')
 def home():
     return render_template('home.html')
 
+#place where ratings will post request to
 @app.route('/addPlace', methods=['POST'])
 def addPlace():
+    placeName = request.form['name']
+    masksRating = request.form['masks']
+    distancingRating = request.form['distancing']
+    outdoorRating = request.form['outdoor']
+    capacityRating = request.form['capacity']
+    contactRating = request.form['contact']
+
+    session['places'] = [
+        {'name': placeName, 
+        'masksRating': masksRating, 
+        'distancingRating': distancingRating, 
+        'outdoorRating': outdoorRating,
+        'capacityRating': capacityRating,
+        'contactRating': contactRating
+        }]
     return redirect('/home')
